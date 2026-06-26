887ba97e5e430b14992bbb5a8e8a76a274f905bf
shahvishva2005-prog <shahvishva2005@gmail.com>
2026-06-26 22:58:36 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-import FlightSearch from "./pages/FlightSearch";
+import FlightSearch from "./pages/FlightSearchPage";
 import SeatSelection from "./pages/SeatSelection";
 import Billing from "./pages/Billing";
 import Payment from "./pages/Payment";
