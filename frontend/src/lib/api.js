c003682b1b102242d73d5692fb78f76607f5ae45
shahvishva2005-prog <shahvishva2005@gmail.com>
2026-06-28 22:18:47 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:10000";
 export const API = `${BACKEND_URL}/api`;
 
 export const api = axios.create({ baseURL: API });
